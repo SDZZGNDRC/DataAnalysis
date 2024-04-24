788df849a8653ef32c4b8ca5431403386fe5980f
SDZZGNDRC <705258871@qq.com>
2024-04-24 08:58:48 +0800

    [update] add two test cases for BLCSI

--- indicators/test_BLCSI.py
+++ indicators/test_BLCSI.py
@@ -5,7 +5,7 @@ import os
 from .BLCSI import BLCSI
 
 
-def test_case_1():
+def test_case_0():
     path = Path(os.path.abspath(__file__)).parent.parent / 'test' / 'SINE-USDT'
     start, end, step = 1000, 149000, 1000
     blcsi = BLCSI(
@@ -15,7 +15,28 @@ def test_case_1():
     )
     assert all([blcsi[i*step+start] == 10 for i in range(len(blcsi))])
 
+def test_case_1():
+    path = Path(os.path.abspath(__file__)).parent.parent / 'test' / 'CASE1-USDT'
+    start, end, step = 1000, 2000, 1000
+    blcsi = BLCSI(
+        instId='CASE1-USDT',
+        start=start, end=end, 
+        path=path, step=step
+    )
+    assert all([blcsi[i*step+start] == 5 for i in range(len(blcsi))])
 
+
+def test_case_2():
+    instId = 'CASE2-USDT'
+    path = Path(os.path.abspath(__file__)).parent.parent / 'test' / instId
+    start, end, step = 1000, 2000, 1000
+    blcsi = BLCSI(
+        instId=instId,
+        start=start, end=end, 
+        path=path, step=step
+    )
+    assert blcsi[1000] == 11
+    assert blcsi[2000] == 6
 
 
 
--- random_books_generator.py
+++ random_books_generator.py
@@ -6,24 +6,10 @@ import sys
 from scipy import signal
 from pathlib import Path
 
-def random_books_generator( px: List[float], 
-                            tss: List[int], 
-                            path: str, 
-                            instId: str = 'TEST-USDT',
-                            size: float = 1.0,
-                            len_bls: int = 5) -> None:
-    if len(px) != len(tss):
-        raise ValueError('Length of px and tss should be the same')
-    columns = ['price', 'size', 'numOrders', 'side', 'action', 'timestamp', 'instId']
-    px = [float(x) for x in px]
-    tss = sorted([int(x) for x in tss])
-    size = float(size)
-    
-    books: Dict[int, List] = {}
-    books[tss[0]] = list(map(lambda x: [float(px[0]+1*x), size, 1, 'ask' if x>=0 else 'bid', 'snapshot', tss[0], instId], [i for i in range(1, len_bls+1)]+[-1*i for i in range(1, len_bls+1)]))
-    for t, ts in enumerate(tss[1:]):
-        books[ts] = list(map(lambda x: [float(px[1:][t]+1*x), size, 1, 'ask' if x>=0 else 'bid', 'update', ts, instId], [i for i in range(1, len_bls+1)]+[-1*i for i in range(1, len_bls+1)]))
 
+def books_generator(path: str, books: Dict[int, List]) -> None:
+    # TODO: Need support to repeated book-levels
+    columns = ['price', 'size', 'numOrders', 'side', 'action', 'timestamp', 'instId']
     tmp_books = sorted(books.items(), key=lambda x:x[0])
     new_books: Dict[int, List] = {}
     for i in range(1, len(tmp_books)):
@@ -31,7 +17,8 @@ def random_books_generator( px: List[float],
         ts = tmp_books[i][0]
         clear_bls = []
         for last_bl in tmp_books[i-1][1]:
-            if (last_bl[0], last_bl[3]) not in px_with_side:
+            # TODO: Maybe can use binary search here?
+            if (last_bl[0], last_bl[3]) not in px_with_side:    # remove this book level
                 updated_bl = last_bl.copy()
                 updated_bl[1] = 0
                 updated_bl[4] = 'update'
@@ -46,7 +33,28 @@ def random_books_generator( px: List[float],
     df.to_parquet(path, compression='gzip', index=False)
 
 
-def sine_books(path: str) -> None:
+def random_books_generator( px: List[float], 
+                            tss: List[int], 
+                            path: str, 
+                            instId: str = 'TEST-USDT',
+                            size: float = 1.0,
+                            len_bls: int = 5) -> None:
+    if len(px) != len(tss):
+        raise ValueError('Length of px and tss should be the same')
+    px = [float(x) for x in px]
+    tss = sorted([int(x) for x in tss])
+    size = float(size)
+    
+    books: Dict[int, List] = {}
+    # price: float, size: float, numOrders: int, side: str, action: str, timestamp: int, instId: str
+    books[tss[0]] = list(map(lambda x: [float(px[0]+1*x), size, 1, 'ask' if x>=0 else 'bid', 'snapshot', tss[0], instId], [i for i in range(1, len_bls+1)]+[-1*i for i in range(1, len_bls+1)]))
+    for t, ts in enumerate(tss[1:]):
+        books[ts] = list(map(lambda x: [float(px[1:][t]+1*x), size, 1, 'ask' if x>=0 else 'bid', 'update', ts, instId], [i for i in range(1, len_bls+1)]+[-1*i for i in range(1, len_bls+1)]))
+
+    books_generator(path, books)
+
+
+def BG_sine(path: str) -> None:
     x = np.arange(0, 2*np.pi, 0.01)
     y = 1000+np.sin(x)*100
 
@@ -69,7 +77,7 @@ def sine_books(path: str) -> None:
     # dump as a png picture
     plt.savefig('Sine.png')
 
-def triangle_books(path: str, sample_points: int = 1000, cycles: int = 3) -> None:
+def BG_triangle(path: str, sample_points: int = 1000, cycles: int = 3) -> None:
     t = np.linspace(0, 1, sample_points)
     px = (1000 + 100*signal.sawtooth(2 * np.pi * cycles * t, 0.5)).round(1)
     tss = [ _ for _ in range(0, len(t)*1000, 1000) ]
@@ -78,7 +86,7 @@ def triangle_books(path: str, sample_points: int = 1000, cycles: int = 3) -> Non
         path.mkdir()
     random_books_generator(
         px, tss, 
-        path/rf'part-0-0-{tss[-1]}.parquet',
+        path / rf'part-0-0-{tss[-1]}.parquet',
         instId='TRIANGLE-USDT'
         )
     # Plot the triangle function
@@ -91,17 +99,126 @@ def triangle_books(path: str, sample_points: int = 1000, cycles: int = 3) -> Non
     plt.savefig('Triangle.png')
 
 
+def BG_case1(path: str) -> None:
+    books: Dict[int, List] = {}
+    # price: float, size: float, numOrders: int, side: str, action: str, timestamp: int, instId: str
+    
+    # ts: 0
+    books[0] = [
+        [100.0, 0.5, 2, 'ask', 'snapshot', 0, 'CASE1-USDT'],
+        [101.0, 0.4, 2, 'ask', 'snapshot', 0, 'CASE1-USDT'],
+        [102.0, 0.3, 2, 'ask', 'snapshot', 0, 'CASE1-USDT'],
+        [103.0, 0.2, 2, 'ask', 'snapshot', 0, 'CASE1-USDT'],
+        [104.0, 0.1, 2, 'ask', 'snapshot', 0, 'CASE1-USDT'],
+        [ 99.0, 0.5, 2, 'bid', 'snapshot', 0, 'CASE1-USDT'],
+        [ 98.0, 0.4, 2, 'bid', 'snapshot', 0, 'CASE1-USDT'],
+        [ 97.0, 0.3, 2, 'bid', 'snapshot', 0, 'CASE1-USDT'],
+        [ 96.0, 0.2, 2, 'bid', 'snapshot', 0, 'CASE1-USDT'],
+        [ 95.0, 0.1, 2, 'bid', 'snapshot', 0, 'CASE1-USDT'],
+    ]
+    
+    # ts: 1000
+    books[1000] = [
+        [100.0, 0.1, 2, 'ask', 'update', 1000, 'CASE1-USDT'],   # changed
+        [101.0, 1.4, 2, 'ask', 'update', 1000, 'CASE1-USDT'],   # changed
+        [102.0, 0.3, 1, 'ask', 'update', 1000, 'CASE1-USDT'],   # changed
+        [103.0, 0.7, 3, 'ask', 'update', 1000, 'CASE1-USDT'],   # changed
+        [104.0, 0.5, 2, 'ask', 'update', 1000, 'CASE1-USDT'],   # changed
+        [ 99.0, 0.5, 2, 'bid', 'update', 1000, 'CASE1-USDT'],   
+        [ 98.0, 0.4, 2, 'bid', 'update', 1000, 'CASE1-USDT'],   
+        [ 97.0, 0.3, 2, 'bid', 'update', 1000, 'CASE1-USDT'],
+        [ 96.0, 0.2, 2, 'bid', 'update', 1000, 'CASE1-USDT'],
+        [ 95.0, 0.1, 2, 'bid', 'update', 1000, 'CASE1-USDT'],
+    ]
+    
+    # ts: 2000
+    books[2000] = [
+        [100.0, 0.1, 2, 'ask', 'update', 2000, 'CASE1-USDT'],
+        [101.0, 1.4, 2, 'ask', 'update', 2000, 'CASE1-USDT'],
+        [102.0, 0.3, 1, 'ask', 'update', 2000, 'CASE1-USDT'],
+        [103.0, 0.7, 3, 'ask', 'update', 2000, 'CASE1-USDT'],
+        [104.0, 0.5, 2, 'ask', 'update', 2000, 'CASE1-USDT'],
+        [ 99.0, 1.5, 1, 'bid', 'update', 2000, 'CASE1-USDT'],   # changed   
+        [ 98.0, 3.4, 2, 'bid', 'update', 2000, 'CASE1-USDT'],   # changed   
+        [ 97.0, 2.3, 4, 'bid', 'update', 2000, 'CASE1-USDT'],   # changed
+        [ 96.0, 1.2, 3, 'bid', 'update', 2000, 'CASE1-USDT'],   # changed
+        [ 95.0, 0.3, 1, 'bid', 'update', 2000, 'CASE1-USDT'],   # changed
+    ]
+    t_path = Path(path) / 'CASE1-USDT'
+    t_path.mkdir(exist_ok=True)
+    books_generator(t_path / 'part-0-0-2000.parquet', books)
+
+
+def BG_case2(path: str) -> None:
+    instId = 'CASE2-USDT'
+    books: Dict[int, List] = {}
+    # price: float, size: float, numOrders: int, side: str, action: str, timestamp: int, instId: str
+    
+    # ts: 0
+    books[0] = [
+        [100.0, 0.5, 2, 'ask', 'snapshot', 0, instId],
+        [101.0, 0.4, 2, 'ask', 'snapshot', 0, instId],
+        [102.0, 0.3, 2, 'ask', 'snapshot', 0, instId],
+        [103.0, 0.2, 2, 'ask', 'snapshot', 0, instId],
+        [104.0, 0.1, 2, 'ask', 'snapshot', 0, instId],
+        [ 99.0, 0.5, 2, 'bid', 'snapshot', 0, instId],
+        [ 98.0, 0.4, 2, 'bid', 'snapshot', 0, instId],
+        [ 97.0, 0.3, 2, 'bid', 'snapshot', 0, instId],
+        [ 96.0, 0.2, 2, 'bid', 'snapshot', 0, instId],
+        [ 95.0, 0.1, 2, 'bid', 'snapshot', 0, instId],
+    ]
+    
+    # ts: 1000    blcsi = 11
+    books[1000] = [
+        [100.0, 0.1, 2, 'ask', 'update', 1000, instId],   # changed
+        [101.0, 1.4, 2, 'ask', 'update', 1000, instId],   # changed
+        [102.0, 0.3, 1, 'ask', 'update', 1000, instId],   # changed
+        [103.0, 0.7, 3, 'ask', 'update', 1000, instId],   # changed
+        [104.0, 0.5, 2, 'ask', 'update', 1000, instId],   # changed
+        [105.0, 1.1, 1, 'ask', 'update', 1000, instId],   # changed
+        # [ 99.0, 0.0, 2, 'bid', 'update', 1000, instId],   # changed
+        [ 98.0, 0.4, 2, 'bid', 'update', 1000, instId],   # changed
+        [ 97.0, 0.3, 2, 'bid', 'update', 1000, instId],   # changed
+        [ 96.0, 0.2, 2, 'bid', 'update', 1000, instId],   # changed
+        [ 95.0, 0.1, 2, 'bid', 'update', 1000, instId],   # changed
+    ]
+    
+    # ts: 2000    blcsi = 6
+    books[2000] = [
+        [100.0, 0.1, 2, 'ask', 'update', 2000, instId],   
+        [101.0, 1.4, 2, 'ask', 'update', 2000, instId],   
+        [102.0, 0.3, 1, 'ask', 'update', 2000, instId],   
+        [103.0, 0.7, 3, 'ask', 'update', 2000, instId],   
+        [104.0, 0.5, 2, 'ask', 'update', 2000, instId],   
+        [105.0, 0.0, 1, 'ask', 'update', 2000, instId],   # changed
+        [ 99.0, 0.2, 1, 'bid', 'update', 2000, instId],   # changed
+        [ 98.0, 0.4, 2, 'bid', 'update', 2000, instId],   # changed
+        [ 97.0, 0.3, 2, 'bid', 'update', 2000, instId],   # changed
+        [ 96.0, 0.2, 2, 'bid', 'update', 2000, instId],   # changed
+        [ 95.0, 0.1, 2, 'bid', 'update', 2000, instId],   # changed
+    ]
+    t_path = Path(path) / instId
+    t_path.mkdir(exist_ok=True)
+    books_generator(t_path / 'part-0-0-2000.parquet', books)
+
+
+
 if __name__ == '__main__':
+    options = {
+        'sine': BG_sine,
+        'triangle': BG_triangle,
+        'case1': BG_case1,
+        'case2': BG_case2,
+    }
     if len(sys.argv)!= 3:
         print('Usage: python3 books_generator.py <book_type> <path>')
-        print('book type: sine, triangle')
+        print(f'book type: {", ".join(options.keys())}')
         exit(-1)
-    if sys.argv[1] =='sine':
-        sine_books(sys.argv[2])
-    elif sys.argv[1] == 'triangle':
-        triangle_books(sys.argv[2])
+    
+    if sys.argv[1] in options:
+        options[sys.argv[1]](sys.argv[2])
     else:
-        print(f'Unknown book type: {sys.argv[1]}')
+        print(f'Unknown options: {sys.argv[1]}')
         exit(-1)
 
 
