91a54d1bf6a54e4eec7170ad9fc6a1546c6b7a22
SDZZGNDRC <sdzzgndrc@gmail.com>
2024-07-24 14:43:28 +0800

    * feat(TV.py): add support for 'side' parameter in TV class constructor * feat(TV.py): add support for 'side' parameter in _calc method * feat(TV.py): modify data attribute to hold both ask and bid data in a single series * feat(TV.py): remove ask and bid methods, add data method to return the series * feat(test_TV.py): update tests to reflect changes in TV class with 'side' parameter and data method

--- indicators/test_TV.py
+++ indicators/test_TV.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pytest
 from pathlib import Path
 import os
+import numpy as np
 
 from .TV import TV
 
@@ -10,19 +11,23 @@ def test_case_1():
     path = Path(os.path.abspath(__file__)).parent.parent / 'test' / instId
     start, end, _ = 0, 2000, 1000
     
-    tv = TV(
+    ask_tv = TV(
         N = 5, instId=instId,
+        side='ask',
         start=start, end=end, path=path
     )
-    tv_gap = tv.gap
+    bid_tv = TV(
+        N = 5, instId=instId,
+        side='bid',
+        start=start, end=end, path=path
+    )
     
-    assert tv[pd.Timestamp(   0, unit='ms')] == (152, 146.5)
-    assert tv[pd.Timestamp(1000, unit='ms')] == (306.09999999999997, 146.5)
-    assert tv[pd.Timestamp(2000, unit='ms')] == (306.09999999999997, 848.5)
-    
-    assert tv_gap[pd.Timestamp(   0, unit='ms')] == 152 - 146.5
-    assert tv_gap[pd.Timestamp(1000, unit='ms')] == 306.09999999999997 - 146.5
-    assert tv_gap[pd.Timestamp(2000, unit='ms')] == 306.09999999999997 - 848.5
+    assert np.isclose(ask_tv[pd.Timestamp(   0, unit='ms')], 152)
+    assert np.isclose(ask_tv[pd.Timestamp(1000, unit='ms')], 306.09999999999997)
+    assert np.isclose(ask_tv[pd.Timestamp(2000, unit='ms')], 306.09999999999997)
+    assert np.isclose(bid_tv[pd.Timestamp(   0, unit='ms')], 146.5)
+    assert np.isclose(bid_tv[pd.Timestamp(1000, unit='ms')], 146.5)
+    assert np.isclose(bid_tv[pd.Timestamp(2000, unit='ms')], 848.5)
 
 
 def test_case_2():
@@ -30,41 +35,47 @@ def test_case_2():
     path = Path(os.path.abspath(__file__)).parent.parent / 'test' / instId
     start, end, _ = 0, 2000, 1000
     
-    tv = TV(
+    ask_tv = TV(
         N = 4, instId=instId,
+        side='ask',
         start=start, end=end, path=path
     )
-    tv_gap = tv.gap
+    bid_tv = TV(
+        N = 4, instId=instId,
+        side='bid',
+        start=start, end=end, path=path
+    )
     
-    assert tv[pd.Timestamp(   0, unit='ms')] == (141.6, 137.0)
-    assert tv[pd.Timestamp(1000, unit='ms')] == (254.09999999999997, 97.0)
-    assert tv[pd.Timestamp(2000, unit='ms')] == (254.09999999999997, 107.3)
-    
-    assert tv_gap[pd.Timestamp(   0, unit='ms')] == 141.6 - 137.0
-    assert tv_gap[pd.Timestamp(1000, unit='ms')] == 254.09999999999997 - 97.0
-    assert tv_gap[pd.Timestamp(2000, unit='ms')] == 254.09999999999997 - 107.3
+    assert np.isclose(ask_tv[pd.Timestamp(   0, unit='ms')], 141.6)
+    assert np.isclose(ask_tv[pd.Timestamp(1000, unit='ms')], 254.09999999999997)
+    assert np.isclose(ask_tv[pd.Timestamp(2000, unit='ms')], 254.09999999999997)
+    assert np.isclose(bid_tv[pd.Timestamp(   0, unit='ms')], 137.0)
+    assert np.isclose(bid_tv[pd.Timestamp(1000, unit='ms')], 97.0)
+    assert np.isclose(bid_tv[pd.Timestamp(2000, unit='ms')], 107.3)
 
 def test_case_3():
     instId = 'CASE3-USDT'
     path = Path(os.path.abspath(__file__)).parent.parent / 'test' / instId
     start, end, _ = 0, 4000, 1000
     
-    tv = TV(
+    ask_tv = TV(
         N = 4, instId=instId,
+        side='ask',
         start=start, end=end, path=path
     )
-    tv_gap = tv.gap
+    bid_tv = TV(
+        N = 4, instId=instId,
+        side='bid',
+        start=start, end=end, path=path
+    )
     
-    assert tv[pd.Timestamp(   0, unit='ms')] == (141.6, 137.0)
-    assert tv[pd.Timestamp(1000, unit='ms')] == (254.09999999999997, 97.0)
-    assert tv[pd.Timestamp(2000, unit='ms')] == (254.09999999999997, 107.3)
-    assert tv[pd.Timestamp(3000, unit='ms')] == (254.09999999999997, 107.3)
-    assert tv[pd.Timestamp(4000, unit='ms')] == (153.2, 78.05)
-    
-    assert tv_gap[pd.Timestamp(   0, unit='ms')] == 141.6 - 137.0
-    assert tv_gap[pd.Timestamp(1000, unit='ms')] == 254.09999999999997 - 97.0
-    assert tv_gap[pd.Timestamp(2000, unit='ms')] == 254.09999999999997 - 107.3
-    assert tv_gap[pd.Timestamp(3000, unit='ms')] == 254.09999999999997 - 107.3
-    assert tv_gap[pd.Timestamp(4000, unit='ms')] == 153.2 - 78.05
-
-
+    assert np.isclose(ask_tv[pd.Timestamp(   0, unit='ms')], 141.6)
+    assert np.isclose(ask_tv[pd.Timestamp(1000, unit='ms')], 254.09999999999997)
+    assert np.isclose(ask_tv[pd.Timestamp(2000, unit='ms')], 254.09999999999997)
+    assert np.isclose(ask_tv[pd.Timestamp(3000, unit='ms')], 254.09999999999997)
+    assert np.isclose(ask_tv[pd.Timestamp(4000, unit='ms')], 153.2)
+    assert np.isclose(bid_tv[pd.Timestamp(   0, unit='ms')], 137.0)
+    assert np.isclose(bid_tv[pd.Timestamp(1000, unit='ms')], 97.0)
+    assert np.isclose(bid_tv[pd.Timestamp(2000, unit='ms')], 107.3)
+    assert np.isclose(bid_tv[pd.Timestamp(3000, unit='ms')], 107.3)
+    assert np.isclose(bid_tv[pd.Timestamp(4000, unit='ms')], 78.05)
