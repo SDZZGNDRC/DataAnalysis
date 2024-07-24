be4800736207e7cec0599990b7369f07941c1fb9
SDZZGNDRC <sdzzgndrc@gmail.com>
2024-07-24 14:27:53 +0800

    * style(AAP.py): refactor AAP class to include a 'side' parameter for ask or bid calculations * test(AAP.py): add tests for AAP class with 'side' parameter for ask and bid calculations

--- indicators/test_AAP.py
+++ indicators/test_AAP.py
@@ -1,6 +1,7 @@
 import pytest
 from pathlib import Path
 import os
+import numpy as np
 
 from .AAP import AAP
 
@@ -9,14 +10,25 @@ def test_case_1():
     path = Path(os.path.abspath(__file__)).parent.parent / 'test' / instId
     start, end, step = 0, 2000, 1000
     
-    aap = AAP(
-        N = 5, instId=instId,
-        start=start, end=end, path=path
+    aap_ask = AAP(
+        N=5, instId=instId,
+        start=start, end=end, path=path,
+        side='ask'
     )
     
-    assert aap[   0] == (102, 97)
-    assert aap[1000] == (102, 97)
-    assert aap[2000] == (102, 97)
+    aap_bid = AAP(
+        N=5, instId=instId,
+        start=start, end=end, path=path,
+        side='bid'
+    )
+    
+    assert np.isclose(aap_ask[0], 102)
+    assert np.isclose(aap_ask[1000], 102)
+    assert np.isclose(aap_ask[2000], 102)
+
+    assert np.isclose(aap_bid[0], 97)
+    assert np.isclose(aap_bid[1000], 97)
+    assert np.isclose(aap_bid[2000], 97)
 
 
 def test_case_2():
@@ -24,29 +36,52 @@ def test_case_2():
     path = Path(os.path.abspath(__file__)).parent.parent / 'test' / instId
     start, end, step = 0, 2000, 1000
     
-    aap = AAP(
-        N = 4, instId=instId,
-        start=start, end=end, path=path
+    aap_ask = AAP(
+        N=4, instId=instId,
+        start=start, end=end, path=path,
+        side='ask'
     )
     
-    assert aap[   0] == (101.5, 97.5)
-    assert aap[1000] == (101.5, 96.5)
-    assert aap[2000] == (101.5, 97.5)
+    aap_bid = AAP(
+        N=4, instId=instId,
+        start=start, end=end, path=path,
+        side='bid'
+    )
+    
+    assert np.isclose(aap_ask[0], 101.5)
+    assert np.isclose(aap_ask[1000], 101.5)
+    assert np.isclose(aap_ask[2000], 101.5)
+
+    assert np.isclose(aap_bid[0], 97.5)
+    assert np.isclose(aap_bid[1000], 96.5)
+    assert np.isclose(aap_bid[2000], 97.5)
+
 
 def test_case_3():
     instId = 'CASE3-USDT'
     path = Path(os.path.abspath(__file__)).parent.parent / 'test' / instId
     start, end, step = 0, 4000, 1000
     
-    aap = AAP(
-        N = 4, instId=instId,
-        start=start, end=end, path=path
+    aap_ask = AAP(
+        N=4, instId=instId,
+        start=start, end=end, path=path,
+        side='ask'
     )
     
-    assert aap[   0] == (101.5, 97.5)
-    assert aap[1000] == (101.5, 96.5)
-    assert aap[2000] == (101.5, 97.5)
-    assert aap[3000] == (101.5, 97.5)
-    assert aap[4000] == (101.75, 97.875)
+    aap_bid = AAP(
+        N=4, instId=instId,
+        start=start, end=end, path=path,
+        side='bid'
+    )
+    
+    assert np.isclose(aap_ask[0], 101.5)
+    assert np.isclose(aap_ask[1000], 101.5)
+    assert np.isclose(aap_ask[2000], 101.5)
+    assert np.isclose(aap_ask[3000], 101.5)
+    assert np.isclose(aap_ask[4000], 101.75)
 
-
+    assert np.isclose(aap_bid[0], 97.5)
+    assert np.isclose(aap_bid[1000], 96.5)
+    assert np.isclose(aap_bid[2000], 97.5)
+    assert np.isclose(aap_bid[3000], 97.5)
+    assert np.isclose(aap_bid[4000], 97.875)
