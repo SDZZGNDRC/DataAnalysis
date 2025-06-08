7aa16ac2cfc4c8238ca4a21b614df85113372d4d
SDZZGNDRC <705258871@qq.com>
2025-06-08 10:05:17 +0800

    [tmp]

--- tail_books.py
+++ tail_books.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import List, Tuple
 from pathlib import Path
 import json
 import numpy as np
@@ -46,7 +47,61 @@ if __name__ == "__main__":
         }
     instId = None
     bc = None
-    first_snapshot_index = -1
+    
+    pfs = iter(pfs)
+    # process first valid pf
+    for pf in pfs:
+        df = pq.ParquetFile(pf).read(columns=['action']).to_pandas()
+        if df.empty:
+            print(f"warning: {pf.name} is empty...")
+            continue
+        if pf.name in metadata['tailed']:
+            print(f"Skipping {pf.name} as it has already been processed.")
+            continue
+        table = pq.read_table(pf)
+        row_group = []
+        schema = table.schema
+        df = table.to_pandas()
+        dps = df.to_dict(orient='records')
+        del df
+        del table
+        writer = pq.ParquetWriter(pf, schema=schema, compression='ZSTD', compression_level=2)
+        group_start = 0
+        for i, dp in enumerate(tqdm(dps, total=len(dps), desc=f"Processing {pf.name}", mininterval=1.0)):
+            if bc == None:
+                if dp['action'] != 'snapshot':
+                    continue
+                instId = dp['arg']['instId']
+                bc = BookCore(instId)
+                group_start = i
+                if i > 0: # useless group
+                    writer.write_batch(pa.RecordBatch.from_pylist(dps[:i], schema=schema), row_group_size=i)
+            try:
+                bc.set_datapoint(dp)
+            except Exception as e:
+                print(f"Error processing datapoint: {e}")
+                print(f"Datapoint: {dp}")
+                raise e
+            if i - group_start >= interval: # should insert a snapshot
+                asks_bl = [[str(bl.price), str(bl.amount), '0', str(bl.count)] for bl in bc.asks]
+                bids_bl = [[str(bl.price), str(bl.amount), '0', str(bl.count)] for bl in bc.bids]
+                # update the row with the new snapshot data
+                dp['data']['asks'] = asks_bl
+                dp['data']['bids'] = bids_bl
+                dp['action'] = 'snapshot'
+                iter_rows = 0
+                row_group.append((group_start, i + 1))
+                group_start = i + 1
+        if group_start < len(dps):
+            row_group.append((group_start, len(dps)))
+        
+        # write the row groups
+        for start, end in row_group:
+            writer.write_batch(pa.RecordBatch.from_pylist(dps[start:end], schema=schema), row_group_size=end-start)
+        writer.close()
+        metadata['tailed'].append(pf.name)
+        update_metadata(metadata, metadata_file)
+        break # only process the first valid file
     for pf in pfs:
         df = pq.ParquetFile(pf).read(columns=['action']).to_pandas()
         if df.empty:
@@ -63,40 +118,36 @@ if __name__ == "__main__":
         del df  # free memory
         del table
         group_start = 0
-        useless_group = 0
-        iter_rows = 0
+        writer = pq.ParquetWriter(pf, schema=schema, compression='ZSTD', compression_level=2)
         for i, dp in enumerate(tqdm(dps, total=len(dps), desc=f"Processing {pf.name}", mininterval=1.0)):
-            if bc == None:
-                if dp['action'] != 'snapshot':
-                    continue
-                instId = dp['arg']['instId']
-                bc = BookCore(instId)
-                group_start = i
-                useless_group = i
-            
             try: 
                 bc.set_datapoint(dp)
             except Exception as e:
                 print(f"Error processing datapoint: {e}")
                 print(f"Datapoint: {dp}")
                 raise e
-            iter_rows += 1
-
-            if iter_rows >= interval: # should insert a snapshot
+            if i == 0: # the first row should be a snapshot
+                if dp['action'] != 'snapshot':
+                    asks_bl = [[str(bl.price), str(bl.amount), '0', str(bl.count)] for bl in bc.asks]
+                    bids_bl = [[str(bl.price), str(bl.amount), '0', str(bl.count)] for bl in bc.bids]
+                    # update the row with the new snapshot data
+                    dp['data']['asks'] = asks_bl
+                    dp['data']['bids'] = bids_bl
+                    dp['action'] = 'snapshot'
+                    group_start = i
+            if i - group_start >= interval: # should insert a snapshot
                 asks_bl = [[str(bl.price), str(bl.amount), '0', str(bl.count)] for bl in bc.asks]
                 bids_bl = [[str(bl.price), str(bl.amount), '0', str(bl.count)] for bl in bc.bids]
                 # update the row with the new snapshot data
                 dp['data']['asks'] = asks_bl
                 dp['data']['bids'] = bids_bl
                 dp['action'] = 'snapshot'
-                iter_rows = 0
-                row_group.append((group_start, i))
+                row_group.append((group_start, i + 1))
                 group_start = i
-        row_group.append((group_start, len(dps)))
-        writer = pq.ParquetWriter(pf, schema=schema, compression='ZSTD', compression_level=2)
-        # first write the useless group
-        if useless_group > 0:
-            writer.write_batch(pa.RecordBatch.from_pylist(dps[:useless_group], schema=schema), row_group_size=useless_group)
+        if group_start < len(dps):
+            row_group.append((group_start, len(dps)))
+        
+        # write the row groups
         for start, end in row_group:
             writer.write_batch(pa.RecordBatch.from_pylist(dps[start:end], schema=schema), row_group_size=end-start)
         writer.close()
