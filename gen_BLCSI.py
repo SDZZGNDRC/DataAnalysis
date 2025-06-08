83970987f0d9660608624141a8d6c5e7e57b6c0a
SDZZGNDRC <705258871@qq.com>
2025-06-08 15:24:01 +0800

    [tmp]

--- gen_BLCSI.py
+++ gen_BLCSI.py
@@ -11,7 +11,7 @@ import os
 # from pybacktest.bookcore import BookCore
 from cbookcore import BookCore, diff
 
-def blcsi(pf_path: Path, group_index: int) -> List[Tuple[int, int, int, int]]:
+def blcsi(pf_path: Path, group_index: int, pfs: List[Path]) -> List[Tuple[int, int, int, int]]:
     pf = pq.ParquetFile(pf_path)
     row_group = pf.read_row_group(group_index)
     df = row_group.to_pandas()
@@ -36,16 +36,35 @@ def blcsi(pf_path: Path, group_index: int) -> List[Tuple[int, int, int, int]]:
         blcsi.append((row.ts, res_asks, res_bids, res_total))
     
     # read next row group to gen the last datapoint
-    if group_index + 1 < pf.num_row_groups: # FIXME: 如果是一个文件的最后一个row_group，实际上可以使用下一个文件的第一个 row_group的第一行生成最后一个数据点。
+    next_df = None
+    if group_index + 1 < pf.num_row_groups:
+        # Next row group is in the same file
         next_row_group = pf.read_row_group(group_index + 1)
         next_df = next_row_group.to_pandas()
-        if not next_df.empty and next_df.iloc[0]['action'] == 'snapshot':
-            # if the next row group has snapshot, we can gen the last datapoint
-            ori_bc = deepcopy(bc)
-            dp = next_df.iloc[0].to_dict()
-            bc.set_datapoint(dp)
-            res_asks, res_bids, res_total = diff(ori_bc, bc)
-            blcsi.append((dp['ts'], res_asks, res_bids, res_total))
+    else:  # It's the last row group, try next file
+        try:
+            current_file_index = pfs.index(pf_path)
+            if current_file_index + 1 < len(pfs):
+                next_pf_path = pfs[current_file_index + 1]
+                try:
+                    next_pf = pq.ParquetFile(next_pf_path)
+                    if next_pf.num_row_groups > 0:
+                        next_row_group = next_pf.read_row_group(0)
+                        next_df = next_row_group.to_pandas()
+                except Exception as e:
+                    print(f"Warning: Could not read metadata from next file {next_pf_path.name}: {e}")
+        except ValueError:
+            # This should not happen if tasks are created correctly.
+            print(f"Warning: {pf_path} not found in the list of files.")
+
+    if next_df is not None and not next_df.empty:
+        # The first row of the next row group is used to calculate the diff.
+        # It doesn't have to be a snapshot.
+        ori_bc = deepcopy(bc)
+        dp = next_df.iloc[0].to_dict()
+        bc.set_datapoint(dp)
+        res_asks, res_bids, res_total = diff(ori_bc, bc)
+        blcsi.append((dp['ts'], res_asks, res_bids, res_total))
     return blcsi
 
 # Wrapper function for multiprocessing that unpacks arguments
@@ -83,7 +102,7 @@ if __name__ == "__main__":
         try:
             pf = pq.ParquetFile(pf_path)
             for i in range(pf.num_row_groups):
-                tasks.append((pf_path, i))
+                tasks.append((pf_path, i, pfs))
         except Exception as e:
             print(f"Could not read metadata from {pf_path.name}: {e}")
     
